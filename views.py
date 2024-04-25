4d65ab600dd0512d5fd37652b0b9b6edbf68141c
Phu Nguyen <ngqp2k@outlook.com>
2024-04-25 12:01:15 +0700

    Update templates and views for room management functionality

--- views.py
+++ views.py
@@ -224,3 +224,59 @@ def delete_room_type(room_type_id):
     db.session.commit()
     
     return redirect(url_for('room_type_page'))
+
+
+@app.route('/room')
+def room_page():
+    rooms = models.Room.query.all()
+    return render_template('mdRoom.html', rooms=rooms)
+
+
+@app.route('/add-room', methods=['GET', 'POST'])
+def add_room():
+    
+    if request.method == 'POST':
+        room = models.Room()
+        room.room_no = request.form['room_no']
+        room.image = request.form['image']
+        room.description = request.form['description']
+        room.room_type = models.RoomType.query.get(request.form['room_type'])
+        
+        db.session.add(room)
+        db.session.commit()
+        
+        return redirect(url_for('room_page'))
+    
+    room_types = models.RoomType.query.all()
+    
+    return render_template('add-room.html'
+                           , room_types=room_types)
+    
+@app.route('/edit-room/<int:room_id>', methods=['GET', 'POST'])
+def edit_room(room_id):
+    room = models.Room.query.get(room_id)
+    
+    if request.method == 'POST':
+        room.room_no = request.form['room_no']
+        room.image = request.form['image']
+        room.description = request.form['description']
+        
+        room_type = models.RoomType.query.get(request.form['room_type'])
+        room.room_type = room_type
+        
+        db.session.commit()
+        
+        return redirect(url_for('room_page'))
+    
+    room_types = models.RoomType.query.all()
+    
+    return render_template('edit-room.html'
+                           , room=room, room_types=room_types)
+    
+@app.route('/delete-room/<int:room_id>', methods=['GET', 'POST'])
+def delete_room(room_id):
+    room = models.Room.query.get(room_id)
+    db.session.delete(room)
+    db.session.commit()
+    
+    return redirect(url_for('room_page'))
